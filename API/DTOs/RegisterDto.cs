d6e71f339856ceb88e2506a8f1c78780b3ae48c2
GiorgosGaramtzidis <giorgosss@live.se>
2023-10-23 11:21:42 +0300

    End of sectionm 12

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -6,6 +6,11 @@ public class RegisterDto
 {
     [Required]
     public string Username { get; set; }
+    [Required] public string KnowsAs { get; set; }
+    [Required] public string Gender { get; set; }
+    [Required] public DateOnly? DateOfBirth { get; set; }
+    [Required] public string City { get; set; }
+    [Required] public string Country { get; set; }
     
     [Required]
     [StringLength(8,MinimumLength = 4)]
